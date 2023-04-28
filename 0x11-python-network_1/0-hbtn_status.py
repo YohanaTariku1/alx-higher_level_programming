3f227e733b6f77f6b15a4f6340c4bf6322bb9ebf
YohanaTariku1 <yohanatariku1@gmail.com>
2023-04-28 09:54:29 +0000

    fixed tabulation for 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,6 +7,6 @@ import urllib.request
 with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
     html = response.read()
     print("Body response:")
-    print(f"    - type: {type(html)}")
-    print(f"    - content: {html}")
-    print(f"    - utf8: {html.decode('utf-8')}")
+    print(f"\t- type: {type(html)}")
+    print(f"\t- content: {html}")
+    print(f"\t- utf8: {html.decode('utf-8')}")
