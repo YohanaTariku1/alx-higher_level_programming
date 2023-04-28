9d31cd313e77c8d11a386fc75243e455155d4de2
YohanaTariku1 <yohanatariku1@gmail.com>
2023-04-28 12:11:41 +0000

    changed syntax

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,4 +7,4 @@ import sys
 import urllib.request
 
 with urllib.request.urlopen(sys.argv[1]) as response:
-    print(response.headers.get('X-Request-Id'))
+    print(response.getheader('X-Request-Id'))
