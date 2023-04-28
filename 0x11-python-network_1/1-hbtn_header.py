d1a1e27f7828c5f67332ae8c80062f4e0bc5e4d9
YohanaTariku1 <yohanatariku1@gmail.com>
2023-04-28 12:08:43 +0000

    changed dictionary retrival to get in python3

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,4 +7,4 @@ import sys
 import urllib.request
 
 with urllib.request.urlopen(sys.argv[1]) as response:
-    print(response.headers['X-Request-Id'])
+    print(response.headers.get('X-Request-Id'))
