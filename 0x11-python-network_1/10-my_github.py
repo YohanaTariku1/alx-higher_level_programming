c722229fc7ef9e870e211cb74b5a10a1df435bef
YohanaTariku1 <yohanatariku1@gmail.com>
2023-05-02 20:03:43 +0000

    added 10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""script that takes your GitHub credentials
-(username and password) and uses the GitHub API
-to display your id
+"""A script that:
+- takes your GitHub credentials (username and password)
+- uses the GitHub API to display your id
 """
-from sys import argv
-from requests.auth import HTTPBasicAuth
+import sys
 import requests
+from requests.auth import HTTPBasicAuth
 
 
-if __name__ == '__main__':
-    url = f"https://api.github.com/users/"
-    r = requests.get(url, auth=HTTPBasicAuth(argv[1], argv[2])) 
-
-    print(r.json().get('id'))
+if __name__ == "__main__":
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id"))
